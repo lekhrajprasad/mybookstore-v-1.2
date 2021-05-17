5e8eb83c2bebc43a5135a92ed05cf12791131040
lekhrajprasad <lekhraj.prasad@gmail.com>
2021-05-17 15:52:14 +0530

    updated

---
+++ cloud-config-server/src/test/java/com/lpras/myconfigserver/StartCloudConfigServerTests.java
@@ -0,0 +1,13 @@
+package com.lpras.myconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartCloudConfigServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ my-bookstore-web/src/test/java/com/lpras/bookstoreweb/StartMyBookStoreWebMSTests.java
@@ -0,0 +1,13 @@
+package com.lpras.bookstoreweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartMyBookStoreWebMSTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
